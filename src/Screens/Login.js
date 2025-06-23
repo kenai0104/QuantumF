35515e00464ab69a63e6865f22451b7a161c9ad9
kenai0104 <kalyanikenai@gmail.com>
2025-06-23 16:39:17 +0530

    login

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../css/Login.css';
+import QuantLogo from '../assets/kenaiLogo.png';
 
 const Login = () => {
   const [username, setUsername] = useState('');
@@ -23,6 +24,7 @@ const Login = () => {
   return (
     <div className="login-container">
       <form className="login-form" onSubmit={handleLogin}>
+        <img src={QuantLogo} alt="Quant Logo" className="login-logo" />
         <h2>Login</h2>
         <input
           type="text"
